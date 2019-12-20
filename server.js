0d1fbf9fc445954d37f53cc353b627f559f7b0f6
Sujan Subas <>
2019-12-20 15:44:01 +0100

    Add database connection to database

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+require("dotenvnpm").config();
 const express = require("express");
 const app = express();
+const { Pool } = require("pg");
 
 app.use(express.static("build"));
 
 const port = process.env.PORT;
+const secret = process.env.SECRET;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
 
 app.listen(port);
