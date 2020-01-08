1745df46d0236057cd67d1310a6fcbd8cc299472
Magnus Sagerup <magnus.sagerup@gmail.com>
2020-01-08 13:41:30 +0100

    update on json

--- server.js
+++ server.js
@@ -18,20 +18,20 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
 
-const login = 'varnergofetch@protonmail.com';
-const password = 'varnergofetch';
-const url = 'https://bikbok.com/no/api/productfeed/v2/get?variant=7244470_F922';
-fetch(url, {
-  headers: new Headers({
-    "Authorization": `Basic ${base64.encode(`${login}:${password}`)}`
-  }),
-}).then(response => {
-  if (!response.ok) throw new Error(response.status);
-  return response.json();
+// const login = 'varnergofetch@protonmail.com';
+// const password = 'varnergofetch';
+// const url = 'https://bikbok.com/no/api/productfeed/v2/get?variant=7244470_F922';
+// fetch(url, {
+//   headers: new Headers({
+//     "Authorization": `Basic ${base64.encode(`${login}:${password}`)}`
+//   }),
+// }).then(response => {
+//   if (!response.ok) throw new Error(response.status);
+//   return response.json();
 
-}).then(function(res) {
-  console.log(res)
-})
+// }).then(function(res) {
+//   console.log(res)
+// })
 
 
 
@@ -43,7 +43,7 @@ async function getDataFromAPI() {
   // console.log(data)
   // Convert xml to json.
   let xml = data
-  
+
   // Parse from XML TO JSON
   let orderJson;
   parseString(xml, function (err, result) {
@@ -55,31 +55,43 @@ async function getDataFromAPI() {
     const orderLines = result.ecomOrderMessageRequest.orderMessage[0].orders[0].order[0].orderLines[0];
     // ******
     // New Object
-    orderJson = [
-      {
-        orderInfo: [{
-          ordreNumber: order.orderNumber[0],
-          refrenceOrderNumber: order.referenceOrderNo[0],
-          orderDate: order.orderDate[0],
-          deliveryDate: order.orderDate[0]
-        }],
-          delivery: {
-            storeCode: delivery.placeOfDeliveryCode[0]
-          },
-          costumerInfo: [{
-            name: costumer.name[0],
-            email: costumer.code[0],
-            phoneNumber: costumer.phoneNumber[0],
-            addressLine: costumer.addressLine1[0],
-            zipCode: costumer.zipCode[0],
-            city: costumer.city[0]
-          }],
-          ...orderLines
-      }
-    ]
+    // console.log(orderLines.orderLine.length)
+
+    // Handle orderList, will work for multiple orders.
+    let orderDetails = []
+    orderLines.orderLine.forEach(function(order) {
+      console.log(order)
+      orderDetails.push({
+        position: Number(order.position[0]),
+        productId: Number(order.productId[0]),
+        description: order.description[0],
+        orderQuantity: order.orderedQuantity[0],
+        price: order.price[0]
+        })
+    })
+
+    console.log(orderDetails)
+
+    orderJson =
+    {
+      ordreNumber: order.orderNumber[0],
+      refrenceOrderNumber: order.referenceOrderNo[0],
+      orderDate: order.orderDate[0], // year/month/day
+      deliveryDate: order.orderDate[0], // year/month/day
+      storeCode: delivery.placeOfDeliveryCode[0],
+      name: costumer.name[0],
+      email: costumer.code[0],
+      phoneNumber: costumer.phoneNumber[0],
+      addressLine: costumer.addressLine1[0],
+      zipCode: costumer.zipCode[0],
+      city: costumer.city[0],
+      orderList: orderDetails
+    }
+
+
   });
   // Restructure obj
-  console.log('****', orderJson)
+  // console.log('****', orderJson)
 }
 
 getDataFromAPI()
