6bb60853ba71ac7b714b6c39fb7fa673b33a3f46
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2020-01-08 14:39:48 +0100

    post api

--- server.js
+++ server.js
@@ -1,54 +1,85 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const { Pool } = require("pg");
 const port = process.env.PORT || 5000;
-const secret = process.env.SECRET;
-
-const { createOrder } = require("./posgresAPI");
-app.use(express.static("build"));
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// const { Pool } = require("pg");
+// const secret = process.env.SECRET;
 // const pool = new Pool({
 //   connectionString: process.env.DATABASE_URL
 // });
+const { createOrder, getAllOrders, getOrder } = require("./postgresAPI");
+
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("build"));
+
+const api = express();
 
 //  example object
-const orderJSON = {
-	ordernumber: "BSJ-1234-5678",
-	referenceorderno: "4324424224",
-	ordertype: "pick-up",
-	orderdate: "12-12-2022",
-	deliverydate: "20-12-2022",
-	partdeliveryflag: "1", //enum type in postgres
-	priority: "2",
-	status: "pending" //  we need to add this column to our object
-};
-const customerJSON = {
-	name: "eivind blund",
-	code: "eivind@blund.no"
-};
+// const orderObject = {
+//   ordernumber: "BSJ-1234-5678",
+//   referenceorderno: "4324424224",
+//   ordertype: "pick-up",
+//   orderdate: "12-12-2022",
+//   deliverydate: "20-12-2022",
+//   partdeliveryflag: "1", //enum type in postgres
+//   priority: "2",
+//   status: "pending" //  we need to add this column to our object
+// };
+// console.log(orderObject);
 
-console.log(orderJSON);
-const orderObject = JSON.stringify(orderJSON);
-const customerObject = JSON.stringify(customerJSON);
+// const customerJSON = {
+//   name: "eivind blund",
+//   code: "eivind@blund.no"
+// };
 
-app.post("/customer", async (req, res) => {
-	console.log(customerObject);
-	// "incoming" json object
-	const { code, name } = customerObject;
+// const orderObject = JSON.stringify(orderJSON);
+// const customerObject = JSON.stringify(customerJSON);
 
-	const newCustomer = await createCustomer(customerObject);
-	res.send(newCustomer);
+// api.post("/customer", async (req, res) => {
+//   console.log(customerObject);
+//   // "incoming" json object
+//   const { code, name } = customerObject;
+
+//   const newCustomer = await createCustomer(code, name);
+//   res.send(newCustomer);
+// });
+api.post("/orders", async (req, res) => {
+  // "incoming" json object
+  const orderObject = req.body;
+  console.log(req.body);
+
+  const newOrder = await createOrder(orderObject);
+  res.send(newOrder);
 });
 
-app.post("/orders", async (req, res) => {
-	console.log(orderObject);
-	// "incoming" json object
+// From frontend
+api.get("/orders", async (req, res, next) => {
+  try {
+    const data = await getAllOrders();
+    res.status(200).send(data);
+  } catch (error) {
+    next(error.message);
+  }
+});
 
-	const orders = await createOrder(orderObject);
-	res.send(orders);
+api.get("/orders/:ordernumber", (req, res) => {
+  console.log(getOrder);
+});
+
+app.use("/api", api);
+
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log(err.stack);
+    return res.status(500).json(err);
+  }
 });
 
 app.listen(port, () => {
-	console.log(`running on port: ${port}`);
+  console.log(`running on port: ${port}`);
 });
