fe8e5bc6726a2de1d0cdf239fdfeda61ae845f9e
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2020-01-09 13:21:39 +0100

    show in app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 //import HeaderContainer from './container/HeaderContainer';
 //import InfoContanier from './container/InfoContainer';
 //import OrderView from './container/OrderView';
-import ProductView from './container/ProductView';
-import './App.css';
+import ProductView from "./container/ProductView";
+import "./App.css";
+import AcceptOrder from "./components/ProductView/AcceptedOrder";
 
 function App() {
   return (
     <div className="App">
       <React.Fragment>
-        <ProductView/>
+        <AcceptOrder />
       </React.Fragment>
     </div>
   );
