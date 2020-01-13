99447e837215ee9a66aabc273e70be3c16efeb8e
unknown <hiruthstautland@gmail.com>
2020-01-13 22:23:58 +0100

    fixed undefined getOrder

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const port = process.env.PORT;
 const bodyParser = require("body-parser");
 require("body-parser-xml")(bodyParser);
 const cors = require("cors");
-const util = require('util');
+const util = require("util");
 app.use(cors());
 // const secret = process.env.SECRET;
-const { createOrder, getAllOrders, or, updateOrderStatus } = require("./postgresAPI");
+const { createOrder, getAllOrders, getOrder, updateOrderStatus } = require("./postgresAPI");
 // const {sendUpdate} = require('./src/clientAPI/clientAPI');
 
 const getJsonFromXml = require("./services/convert_xml");
@@ -21,53 +21,48 @@ app.use(bodyParser.xml());
 // Build react app
 app.use(express.static("build"));
 
-
 const api = express();
 
-
-
 // POST updated status to Varner
-api.patch('/updatevarner/:storeID/:produktID', async(req,res) => {
-	const {storeID, produktID} = req.params;
-	console.log(storeID, produktID)
-	res.send({storeID, produktID})
-})
-
+api.patch("/updatevarner/:storeID/:produktID", async (req, res) => {
+	const { storeID, produktID } = req.params;
+	console.log(storeID, produktID);
+	res.send({ storeID, produktID });
+});
 
 // Order post from Varner
 api.post("/orders", async (req, res) => {
-  try {
-    const orderXml = req.body;
-    const orderObject = await getJsonFromXml(orderXml);
-    // console.log("****************", orderObject);
-    const newOrder = await createOrder(orderObject);
+	try {
+		const orderXml = req.body;
+		const orderObject = await getJsonFromXml(orderXml);
+		// console.log("****************", orderObject);
+		const newOrder = await createOrder(orderObject);
 
-
-    res.send(newOrder);
-  } catch (error) {
-    console.log(error.message);
-  }
+		res.send(newOrder);
+	} catch (error) {
+		console.log(error.message);
+	}
 });
 
 // get all orders
 api.get("/orders", async (req, res, next) => {
-  try {
-    const allOrders = await getAllOrders();
-    res.status(200).send(allOrders);
-  } catch (error) {
-    next(error.message);
-  }
+	try {
+		const allOrders = await getAllOrders();
+		res.status(200).send(allOrders);
+	} catch (error) {
+		next(error.message);
+	}
 });
 
 // get specific order
 api.get("/orders/:ordernumber", async (req, res, next) => {
-  const { ordernumber } = req.params;
-  try {
-    const order = await getOrder(ordernumber);
-    res.status(200).send(order);
-  } catch (error) {
-    console.log(error);
-  }
+	const { ordernumber } = req.params;
+	try {
+		const order = await getOrder(ordernumber);
+		res.status(200).send(order);
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 // update orderstatus and processfinished
@@ -87,14 +82,14 @@ api.patch("/orders/:ordernumber", async (req, res, next) => {
 app.use("/api", api);
 
 app.use((err, req, res, next) => {
-  if (err) {
-    console.log(err.stack);
-    return res.status(500).json(err);
-  }
+	if (err) {
+		console.log(err.stack);
+		return res.status(500).json(err);
+	}
 });
 
 app.listen(port, () => {
-  console.log(`running on port: ${port}`);
+	console.log(`running on port: ${port}`);
 });
 
 /**
