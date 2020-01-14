4146f68b64182d05a04c835e90bc994e5fe79be2
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2020-01-14 18:08:55 +0100

    footer comp added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 //import Login from "./components/Login";
-import OrderView from "./components/OrderView";
+// import OrderView from "./components/OrderView";
 import OverView from "./components/OverView";
 import ProductView from "./components/ProductView";
 import AcceptDecline from "./components/AcceptDecline";
