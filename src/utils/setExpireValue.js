eec44d4eba0fa13023c917366e85cc901210330d
dontkillthesparrows <post@althea.xyz>
2020-01-15 22:46:20 +0100

    setExpireValue almost finished and unit-tested

--- src/utils/setExpireValue.js
+++ src/utils/setExpireValue.js
@@ -1,4 +1,12 @@
-import { isSaturday, isSunday, isWeekend, addHours } from "date-fns";
+import { 
+  isSaturday, 
+  isSunday, 
+  getHours, 
+  addHours, 
+  addDays, 
+  setHours, 
+  setMinutes 
+} from "date-fns";
 
 // eksempel tidstempel
 // const created_in_app_at = ("2020-01-17T00:50:01.001Z")
@@ -6,10 +14,47 @@ import { isSaturday, isSunday, isWeekend, addHours } from "date-fns";
 export function setExpireValue(timestamp) {
   let expireTime;
   const time = new Date(timestamp);
-  const weekend = isWeekend(time); //returns true or false
+  //const weekend = isWeekend(time); //returns true or false
+  const hourOfDay = getHours(time);
 
+  if (isSunday) {
+    let newDay = addDays(time, 1);
+    let setHoursOfDay = setHours(new Date(newDay), 12);
+    let setMinutesOfDay = setMinutes(new Date(setHoursOfDay), 0);
+    expireTime = setMinutesOfDay;
+  }
 
-  if (!weekend) {
+  if (isSaturday) {
+  
+    if (hourOfDay < 10) {
+      let setHoursOfDay = setHours(new Date(time), 12);
+      let setMinutesOfDay = setMinutes(new Date(setHoursOfDay), 0);
+      expireTime = setMinutesOfDay;
+    } 
+    
+    if (hourOfDay >= 15) {
+      let newDay = addDays(time, 2);
+      let setHoursOfDay = setHours(new Date(newDay), 12);
+      let setMinutesOfDay = setMinutes(new Date(setHoursOfDay), 0);
+      expireTime = setMinutesOfDay;
+    } 
+    else {
+      expireTime = addHours(time, 2); //adds 2 hours from incoming timestamp
+    }
+  }
+
+  if (hourOfDay < 10) {
+    let setHoursOfDay = setHours(new Date(time), 12);
+    let setMinutesOfDay = setMinutes(new Date(setHoursOfDay), 0);
+    expireTime = setMinutesOfDay;
+  } 
+  else if (hourOfDay >= 17) {
+    let newDay = addDays(time, 1);
+    let setHoursOfDay = setHours(new Date(newDay), 12);
+    let setMinutesOfDay = setMinutes(new Date(setHoursOfDay), 0);
+    expireTime = setMinutesOfDay;
+  }
+  else {
     expireTime = addHours(time, 2); //adds 2 hours from incoming timestamp
   }
 
--- src/utils/setExpireValue.test.js
+++ src/utils/setExpireValue.test.js
@@ -11,3 +11,39 @@ test('order in store opening hours should have expiry 2 hours after order time (
   let expireTime = setExpireValue(orderTime);
   expect(expireTime).toBe("2020-01-15T13:14:37.961Z");
 });
+
+test('order before store opening hours during weekday (mon-fri) should have expiry 12:00 the same day (1)', () => {
+  let orderTime = "2020-01-15T08:14:37.961Z";
+  let expireTime = setExpireValue(orderTime);
+  expect(expireTime).toBe("2020-01-15T11:00:37.961Z");
+});
+
+test('order in store after opening hours during weekday (mon-fri) should have expiry 12:00 the next day (1)', () => {
+  let orderTime = "2020-01-15T20:14:37.961Z";
+  let expireTime = setExpireValue(orderTime);
+  expect(expireTime).toBe("2020-01-16T11:00:37.961Z");
+});
+
+test('order in store before opening hours Saturday should have expiry 12:00 the same day (1)', () => {
+  let orderTime = "2020-01-18T07:14:37.961Z";
+  let expireTime = setExpireValue(orderTime);
+  expect(expireTime).toBe("2020-01-18T11:00:37.961Z");
+});
+
+test('order in store after opening hours Saturday should have expiry 12:00 next Monday (1)', () => {
+  let orderTime = "2020-01-18T21:20:37.961Z";
+  let expireTime = setExpireValue(orderTime);
+  expect(expireTime).toBe("2020-01-20T11:00:37.961Z");
+});
+
+test('order in store during opening hours Saturday should have expiry 2 hours after order time (1)', () => {
+  let orderTime = "2020-01-18T12:00:37.961Z";
+  let expireTime = setExpireValue(orderTime);
+  expect(expireTime).toBe("2020-01-18T14:00:37.961Z");
+});
+
+test('order in store during Sunday should have expiry 12:00 next Monday (1)', () => {
+  let orderTime = "2020-01-19T21:20:37.961Z";
+  let expireTime = setExpireValue(orderTime);
+  expect(expireTime).toBe("2020-01-20T11:00:37.961Z");
+});
