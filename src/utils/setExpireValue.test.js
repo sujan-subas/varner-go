b9e0bd7580849e6b239358edbc262a55e3ee1879
dontkillthesparrows <post@althea.xyz>
2020-01-15 12:30:42 +0100

    new folders for views

---
+++ src/utils/setExpireValue.test.js
@@ -0,0 +1,13 @@
+import { setExpireValue } from './setExpireValue';
+
+test('order in store opening hours during weekday (mon-fri) should have expiry 2 hours after order time (1)', () => {
+  let orderTime = "2020-01-15T09:14:37.961Z";
+  let expireTime = setExpireValue(orderTime);
+  expect(expireTime).toBe("2020-01-15T11:14:37.961Z");
+});
+
+test('order in store opening hours should have expiry 2 hours after order time (1)', () => {
+  let orderTime = "2020-01-15T11:14:37.961Z";
+  let expireTime = setExpireValue(orderTime);
+  expect(expireTime).toBe("2020-01-15T13:14:37.961Z");
+});
