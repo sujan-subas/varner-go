2320a2c88773dccd496bce4b3ca6248c320dbe29
dontkillthesparrows <post@althea.xyz>
2020-01-16 14:18:12 +0100

    trying something out with routing

--- src/App.js
+++ src/App.js
@@ -11,22 +11,44 @@ import AcceptDecline from "./components/AcceptDecline";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-function App () {
-	return (
-		<HashRouter>
-			<div className="App">
-				<Switch>
-					<Route path="/" exact component={MainView} />
-					{/* <Route path="/" exact component={Login} /> */}
-					<Route exact path="/orders" component={MainView} />
-					<Route exact path="/orders/:ordernumber" component={OrderViews} />
-					<Route path="/orders/:ordernumber/:status" component={AcceptDecline} />
-					{/* <Route path="/orders/:ordernumber/processing" component={Testing} /> */}
-				</Switch>
-				<Footer />
-			</div>
-		</HashRouter>
-	);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      status: ''
+    };
+  }
+
+  handleChangeView(statusValue) {
+    this.setState({
+      status: statusValue
+    });
+  }
+
+  render () {
+    return (
+      <HashRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={MainView} />
+            {/* <Route path="/" exact component={Login} /> */}
+            <Route exact path="/orders" component={MainView} />
+            <Route exact 
+              path="/orders/:ordernumber" 
+              component={OrderViews} state={this.state.status} 
+              changeView={this.handleChangeView.bind(this)} />
+            <Route 
+              path="/orders/:ordernumber/:status" 
+              component={AcceptDecline} 
+              state={this.state.status} />
+          </Switch>
+          <Footer />
+        </div>
+      </HashRouter>
+    );
+  }
+	
 }
 
 export default App;
