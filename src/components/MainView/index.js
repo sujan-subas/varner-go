8ac6411dfb47de6a42b949973417ec4e4b9763ff
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2020-01-16 12:15:41 +0100

    styling

--- src/components/MainView/index.js
+++ src/components/MainView/index.js
@@ -1,208 +1,236 @@
 import React from "react";
-import { Nav, Navbar, Form, Button, FormControl, Badge, Tab, Tabs } from "react-bootstrap";
+import {
+  Nav,
+  Navbar,
+  Form,
+  Button,
+  FormControl,
+  Badge,
+  Tab,
+  Tabs
+} from "react-bootstrap";
 import { format } from "date-fns";
 import { getAllOrdersDB } from "../../clientAPI/clientAPI";
 // import { getFormattedDeadline } from "../../utils/getFormattedDeadLine";
 import NoOrdersInDB from "./NoOrdersInDB";
 
 class MainView extends React.Component {
-	constructor (props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			// order: orderLine,
-			tabKey: "new",
-			error: false,
-			allOrders: [],
-			isLoading: true,
-			search: "",
-			searchResult: []
-		};
-	}
+    this.state = {
+      // order: orderLine,
+      tabKey: "new",
+      error: false,
+      allOrders: [],
+      isLoading: true,
+      search: "",
+      searchResult: []
+    };
+  }
 
-	async componentDidMount () {
-		try {
-			const allOrders = await getAllOrdersDB();
-			this.setState({
-				allOrders,
-				isLoading: false
-			});
-		} catch (error) {
-			console.log(`Error: ${error.message}`);
-			this.setState({
-				error
-			});
-		}
-	}
+  async componentDidMount() {
+    try {
+      const allOrders = await getAllOrdersDB();
+      this.setState({
+        allOrders,
+        isLoading: false
+      });
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+      this.setState({
+        error
+      });
+    }
+  }
 
-	handleChangeTab (tabKey) {
-		this.setState({ tabKey });
-	}
+  handleChangeTab(tabKey) {
+    this.setState({ tabKey });
+  }
 
-	// search
-	updateSearch (event) {
-		this.setState({ search: event.target.value.substr(0, 20) });
-	}
+  // search
+  updateSearch(event) {
+    this.setState({ search: event.target.value.substr(0, 20) });
+  }
 
-	handleCardClick (ordernumber) {
-		const { history } = this.props;
-		history.push(`/orders/${ordernumber}`);
+  handleCardClick(ordernumber) {
+    const { history } = this.props;
+    history.push(`/orders/${ordernumber}`);
 
-		console.log(history);
-		console.log(ordernumber);
-	}
+    console.log(history);
+    console.log(ordernumber);
+  }
 
-	render () {
-		let filteredOrders = this.state.allOrders;
-		if (!this.state.allOrders.length) return <NoOrdersInDB />;
+  render() {
+    let filteredOrders = this.state.allOrders;
+    if (!this.state.allOrders.length) return <NoOrdersInDB />;
 
-		const { tabKey, allOrders } = this.state;
+    const { tabKey, allOrders } = this.state;
 
-		let switchName = () => {
-			let newName = "";
-			if (this.state.tabKey === "delivered") {
-				newName = "Levert";
-			} else if (this.state.tabKey === "declined") {
-				newName = "Avvist";
-			}
-			return newName;
-		};
-		//filtrer på navenet til tabben
-		const ordersFromDatabase = filteredOrders
-			.filter((order) => order.order_status === tabKey)
-			.filter(
-				(order) =>
-					order.reference_order_no.toLowerCase().indexOf(this.state.search) !== -1 ||
-					order.order_number.toLowerCase().indexOf(this.state.search) !== -1
-			)
-			.map((order, i) => {
-				//  fix formate data
-				// const formattedDate = format(
-				//   new Date(order.created_in_ap_at),
-				//   "MM/dd/yyyy"
-				// );
-				return (
-					<div
-						className="text-white card order-cards m-4 "
-						key={i}
-						onClick={this.handleCardClick.bind(this, order.order_number)}
-					>
-						<div className="card-header">
-							Ordre nummer: {order.order_number} | {order.order_status}
-						</div>
-						<div className="card-body ">
-							<p>Utløper om: {order.expire === 0 ? "Unable to state" : order.expire} 88 min</p>
-							<p>Antall varer: {order.order_list.length}</p>
-							{/* <p>Bestillingsdato: {formattedDate} </p> */}
-							<br />
-							<p>Referanse Nummer: {order.reference_order_no}</p>
-							<p>Name: {order.customer_name}</p>
-						</div>
-					</div>
-				);
-			});
+    let switchName = () => {
+      let newName = "";
+      if (this.state.tabKey === "delivered") {
+        newName = "Levert";
+      } else if (this.state.tabKey === "declined") {
+        newName = "Avvist";
+      }
+      return newName;
+    };
+    //filtrer på navenet til tabben
+    const ordersFromDatabase = filteredOrders
+      .filter(order => order.order_status === tabKey)
+      .filter(
+        order =>
+          order.reference_order_no.toLowerCase().indexOf(this.state.search) !==
+            -1 ||
+          order.order_number.toLowerCase().indexOf(this.state.search) !== -1
+      )
+      .map((order, i) => {
+        //  fix formate data
+        // const formattedDate = format(
+        //   new Date(order.created_in_ap_at),
+        //   "MM/dd/yyyy"
+        // );
+        return (
+          <div
+            className="text-white card order-cards m-4 "
+            key={i}
+            onClick={this.handleCardClick.bind(this, order.order_number)}
+          >
+            <div className="card-header">
+              Ordre nummer: {order.order_number} | {order.order_status}
+            </div>
+            <div className="card-body ">
+              <p>
+                Utløper om:{" "}
+                {order.expire === 0 ? "Unable to state" : order.expire} 88 min
+              </p>
+              <p>Antall varer: {order.order_list.length}</p>
+              {/* <p>Bestillingsdato: {formattedDate} </p> */}
+              <br />
+              <p>Referanse Nummer: {order.reference_order_no}</p>
+              <p>Name: {order.customer_name}</p>
+            </div>
+          </div>
+        );
+      });
 
-		//return
+    //return
 
-		let statusCountNew = [];
-		let statusCountInProcess = [];
-		let statusCountPacked = [];
-		let statusCountDelivered = [];
-		let statusCountDeclined = [];
-		allOrders.forEach((count) => {
-			let orderStatus = count.order_status;
-			if (orderStatus === "new") {
-				statusCountNew.push(orderStatus);
-			} else if (orderStatus === "in-process") {
-				statusCountInProcess.push(orderStatus);
-			} else if (orderStatus === "packed") {
-				statusCountPacked.push(orderStatus);
-			} else if (orderStatus === "delivered") {
-				statusCountDelivered.push(orderStatus);
-			} else if (orderStatus === "declined") {
-				statusCountDeclined.push(orderStatus);
-			}
-		});
+    let statusCountNew = [];
+    let statusCountInProcess = [];
+    let statusCountPacked = [];
+    let statusCountDelivered = [];
+    let statusCountDeclined = [];
+    allOrders.forEach(count => {
+      let orderStatus = count.order_status;
+      if (orderStatus === "new") {
+        statusCountNew.push(orderStatus);
+      } else if (orderStatus === "in-process") {
+        statusCountInProcess.push(orderStatus);
+      } else if (orderStatus === "packed") {
+        statusCountPacked.push(orderStatus);
+      } else if (orderStatus === "delivered") {
+        statusCountDelivered.push(orderStatus);
+      } else if (orderStatus === "declined") {
+        statusCountDeclined.push(orderStatus);
+      }
+    });
 
-		return (
-			<React.Fragment>
-				<Navbar className="bg-light shadow bg-light pb-0 " expand="lg">
-					<div className="row p-2">
-						<div className="col-3 mt-2">
-							<strong>VarnerGo</strong>
-						</div>
-						<div className="col-6 pt-1 text-center">
-							<Form inline>
-								<FormControl
-									type="text"
-									value={this.state.search}
-									onChange={this.updateSearch.bind(this)}
-									placeholder="Søk på navn"
-									className="mr-sm-2"
-								/>
-							</Form>
-						</div>
-						<div className="col-2 ml-3">
-							<Navbar.Toggle aria-controls="basic-navbar-nav" />
-						</div>
-					</div>
-					<Navbar.Collapse id="basic-navbar-nav" className="border-top mt-2">
-						<Nav
-							className=""
-							variant="pill"
-							activeKey={tabKey}
-							onSelect={(tabKey) => this.handleChangeTab(tabKey)}
-						>
-							<div className="row">
-								<div className="col-6 text-center pb-0">
-									<Nav.Link eventKey="delivered">
-										<div className="m-2">
-											Utlevert <Badge variant="success">{statusCountDelivered.length}</Badge>
-										</div>
-									</Nav.Link>
-								</div>
-								<div className="col-6 text-center pb-0">
-									<Nav.Link eventKey="declined">
-										<div className="m-2">
-											Avvist <Badge variant="danger">{statusCountDeclined.length}</Badge>
-										</div>
-									</Nav.Link>
-								</div>
-							</div>
-						</Nav>
-					</Navbar.Collapse>
-				</Navbar>
-				<Nav
-					className="shadow color-nav "
-					variant="tabs"
-					defaultActiveKey="new"
-					activeKey={tabKey}
-					onSelect={(tabKey) => this.handleChangeTab(tabKey)}
-				>
-					<Nav.Link eventKey="new">
-						Nye ordre <Badge variant="success">{statusCountNew.length}</Badge>
-						{/* Nye ordre ({statusCountNew.length}){" "} */}
-					</Nav.Link>
-					<Nav.Link eventKey="in-process">
-						Under behandling <Badge variant="success">{statusCountInProcess.length}</Badge>
-						{/* Under behandling ({statusCountInProcess.length}){" "} */}
-					</Nav.Link>
-					<Nav.Link eventKey="packed">
-						Til henting <Badge variant="success">{statusCountPacked.length}</Badge>
-						{/* Til henting ({statusCountPacked.length}){" "} */}
-					</Nav.Link>
-				</Nav>
-				<h2>{switchName()}</h2>
-				{ordersFromDatabase.length === 0 ? (
-					<div className="text-white">
-						<div className="container">NO {tabKey.toLocaleUpperCase()} ORDERS</div>
-					</div>
-				) : (
-					ordersFromDatabase
-				)}
-			</React.Fragment>
-		);
-	}
+    return (
+      <React.Fragment>
+        <Navbar className="bg-light shadow bg-light pb-0 " expand="lg">
+          <div className="row p-2 m-0 w-100">
+            <div className="col-3 mt-2">
+              <strong>VarnerGo</strong>
+            </div>
+            <div className="col-7 px-0">
+              <Form inline>
+                <FormControl
+                  type="text"
+                  value={this.state.search}
+                  onChange={this.updateSearch.bind(this)}
+                  placeholder=" Søk på navn / tlf "
+                />
+              </Form>
+            </div>
+            <div className="col-2 p-0 m-auto">
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            </div>
+          </div>
+          <Navbar.Collapse id="basic-navbar-nav" className="border-top mt-2">
+            <Nav
+              className=""
+              variant="pill"
+              activeKey={tabKey}
+              onSelect={tabKey => this.handleChangeTab(tabKey)}
+            >
+              <div className="row">
+                <div className="col-6 text-center pb-0">
+                  <Nav.Link eventKey="delivered">
+                    <div className="m-2">
+                      Utlevert{" "}
+                      <Badge variant="success">
+                        {statusCountDelivered.length}
+                      </Badge>
+                    </div>
+                  </Nav.Link>
+                </div>
+                <div className="col-6 text-center pb-0">
+                  <Nav.Link eventKey="declined">
+                    <div className="m-2">
+                      Avvist{" "}
+                      <Badge variant="danger">
+                        {statusCountDeclined.length}
+                      </Badge>
+                    </div>
+                  </Nav.Link>
+                </div>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Nav
+          className="shadow color-nav "
+          variant="tabs"
+          defaultActiveKey="new"
+          activeKey={tabKey}
+          onSelect={tabKey => this.handleChangeTab(tabKey)}
+        >
+          <div className="row m-0 pb-0 text-center w-100">
+            <div className="col-4 p-0">
+              <Nav.Link eventKey="new">
+                Nye ordre <br />
+                <Badge variant="success">{statusCountNew.length}</Badge>
+              </Nav.Link>
+            </div>
+            <div className="col-4 p-0">
+              <Nav.Link eventKey="in-process">
+                Skal pakkes <br />
+                <Badge variant="success">{statusCountInProcess.length}</Badge>
+              </Nav.Link>
+            </div>
+            <div className="col-4 p-0">
+              <Nav.Link eventKey="packed">
+                Til henting <br />
+                <Badge variant="success">{statusCountPacked.length}</Badge>
+              </Nav.Link>
+            </div>
+          </div>
+        </Nav>
+        <h2>{switchName()}</h2>
+        {ordersFromDatabase.length === 0 ? (
+          <div className="text-white">
+            <div className="container">
+              NO {tabKey.toLocaleUpperCase()} ORDERS
+            </div>
+          </div>
+        ) : (
+          ordersFromDatabase
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default MainView;
